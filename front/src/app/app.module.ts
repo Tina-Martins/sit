6d248f96e5bc6e5b64bee082979592f5a3554816
Victor <victor.henrique5800@gmail.com>
2024-03-18 07:06:31 -0300

    Setup inicial

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
