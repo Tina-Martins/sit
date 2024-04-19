33e9d43d89d38261f91685099443c5677c93d3a5
Victor <victor.henrique5800@gmail.com>
2024-04-19 11:49:39 -0300

    Componentes básicos adicionados

---
+++ front/src/app/base-de-dados/ficha-abrigamento/ficha-abrigamento.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ficha-abrigamento',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ficha-abrigamento.component.html',
+  styleUrls: ['./ficha-abrigamento.component.scss']
+})
+export class FichaAbrigamentoComponent {
+
+}
---
+++ front/src/app/base-de-dados/ficha/ficha.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ficha',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ficha.component.html',
+  styleUrls: ['./ficha.component.scss']
+})
+export class FichaComponent {
+
+}
