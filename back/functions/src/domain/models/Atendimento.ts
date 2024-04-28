9091b4e3aa97cda0d7f33abd6eeab2c7f5f23ffe
Henrique <hlisboasousa@gmail.com>
2024-04-28 15:01:02 -0300

    adicionando models

---
+++ back/functions/src/domain/models/Atendimento.ts
@@ -0,0 +1,10 @@
+import { FieldValue } from "firebase-admin/firestore";
+
+export interface Atendimento {
+    id?: string;
+    data: FieldValue | Date;
+    registro: string;
+    criadoEm: FieldValue | Date;
+    atualizadoEm: FieldValue | Date;
+    regAtivo: boolean;
+}
---
+++ back/functions/src/domain/models/Enums/DemandaEnums.ts
@@ -0,0 +1,6 @@
+export enum DemandaStatus {
+    EM_ABERTO = 'EM_ABERTO',
+    EM_ANDAMENTO = 'EM_ANDAMENTO',
+    ENCERRADO = 'ENCERRADO',
+    ENCAMINHADO = 'ENCAMINHADO'
+}
