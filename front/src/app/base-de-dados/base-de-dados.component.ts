1a144d67fb43b71d83f2cd4ea1644f2f9a226951
Victor <victor.henrique5800@gmail.com>
2024-05-07 16:38:11 -0300

    Implementando base-de-dados

--- front/src/app/base-de-dados/base-de-dados.component.ts
+++ front/src/app/base-de-dados/base-de-dados.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-base-de-dados',
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatInputModule, MatFormFieldModule],
+  imports: [CommonModule],
   templateUrl: './base-de-dados.component.html',
   styleUrls: ['./base-de-dados.component.scss']
 })
